b3a8ab8fcb0cfefd5e1874f2e2e3da309cc432d2
Sezer <Sezer@SEZER-THINKPAD>
2021-02-18 16:13:26 +0300

    kodlama.io 9.ders sonu

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            this.categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            return categoryDal.GetAll();
+        }
+
+        public Category GetByCategoryId(int categoryid )
+        {
+            return categoryDal.Get(c => c.CategoryId == categoryid);
+        }
+    }
+}
